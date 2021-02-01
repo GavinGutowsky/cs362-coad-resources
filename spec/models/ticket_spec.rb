bfd81b2b48ee0a712c673e367843d49d630f7daf
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-01 10:43:40 -0800

    Creation of attribute tests for Ticket model

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,8 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-  it "exists" do
-    Ticket.new
+  it "has a name" do
+    expect(Ticket.new).to respond_to(:name)
+  end
+
+  it "has a description" do
+    expect(Ticket.new).to respond_to(:description)
+  end
+
+  it "has a phone" do
+    expect(Ticket.new).to respond_to(:phone)
+  end
+
+  it "has a closed attribute" do
+    expect(Ticket.new).to respond_to(:closed)
+  end
+
+  it "has a closed_at attribute" do
+    expect(Ticket.new).to respond_to(:closed_at)
   end
 
 end
