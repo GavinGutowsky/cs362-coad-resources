8fe3ea32a4cd342501b4ea9d2d728c0e5793524b
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-01 10:32:06 -0800

    Creation of ResourceCategory model attribute tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
-  it "exists" do
-    ResourceCategory.new
+  it "has a name" do
+    expect(ResourceCategory.new).to respond_to(:name)
+  end
+
+  it "has an active attribute" do
+    expect(ResourceCategory.new).to respond_to(:active)
   end
 
 end
