752d00c4cb723a444e77ac6c19856a057aba0458
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-01 10:09:34 -0800

    Creation of Organization model attribute tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,8 +2,69 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
-  it "exists" do
-    Organization.new
+=begin
+  describe "attributes" do
+    it { is_expected.to respond_to(:name) }
+    it { is_expected.to respond_to(:status) }
+    it { is_expected.to respond_to(:phone) }
+    it { is_expected.to respond_to(:email) }
+    it { is_expected.to respond_to(:description) }
+    it { is_expected.to respond_to(:rejection_reason) }
+    it { is_expected.to respond_to(:liability_insurance) }
+    it { is_expected.to respond_to(:primary_name) }
+    it { is_expected.to respond_to(:secondary_name) }
+    it { is_expected.to respond_to(:secondary_phone) }
+    it { is_expected.to respond_to(:title) }
+    it { is_expected.to respond_to(:transportation) }
+  end
+=end
+
+  it "has a name" do
+    expect(Organization.new).to respond_to(:name)
+  end
+
+  it "has a status" do
+    expect(Organization.new).to respond_to(:status)
+  end
+
+  it "has a phone" do
+    expect(Organization.new).to respond_to(:phone)
+  end
+
+  it "has an email" do
+    expect(Organization.new).to respond_to(:email)
+  end
+
+  it "has a description" do
+    expect(Organization.new).to respond_to(:description)
+  end
+
+  it "has a rejection reason" do
+    expect(Organization.new).to respond_to(:rejection_reason)
+  end
+
+  it "has a liability insurance attribute" do
+    expect(Organization.new).to respond_to(:liability_insurance)
+  end
+
+  it "has a primary name" do
+    expect(Organization.new).to respond_to(:primary_name)
+  end
+
+  it "has a secondary name" do
+    expect(Organization.new).to respond_to(:secondary_name)
+  end
+
+  it "has a secondary phone" do
+    expect(Organization.new).to respond_to(:secondary_phone)
+  end
+
+  it "has a title" do
+    expect(Organization.new).to respond_to(:title)
+  end
+
+  it "has a transportation attribute" do
+    expect(Organization.new).to respond_to(:transportation)
   end
 
 end
