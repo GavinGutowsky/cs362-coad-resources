1dd7a0a4aa0e0e6f01c022266a2100bf8ca67489
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-01 20:38:52 -0800

    Refactor ResourceCategory model tests to DRY object instantiation

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,20 +2,14 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
-  it "has a name" do
-    expect(ResourceCategory.new).to respond_to(:name)
+  describe "attributes" do
+    it { is_expect.to respond_to(:name) }
+    it { is_expect.to respond_to(:active) }
   end
 
-  it "has an active attribute" do
-    expect(ResourceCategory.new).to respond_to(:active)
-  end
-
-  it "has and belongs to many organizations" do
-    expect(ResourceCategory.new).to have_and_belong_to_many(:organizations)
-  end
-
-  it "has many tickets" do
-    expect(ResourceCategory.new).to have_many(:tickets)
+  describe "associations" do
+    it { is_expect.to have_and_belong_to_many(:organizations) }
+    it { is_expect.to have_many(:tickets) }
   end
 
 end
