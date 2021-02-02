78f9912bf0dd86694563e879462460e7514e0358
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-01 17:57:28 -0800

    ResourceCategory model association tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe ResourceCategory, type: :model do
     expect(ResourceCategory.new).to respond_to(:active)
   end
 
+  it "has and belongs to many organizations" do
+    expect(ResourceCategory.new).to have_and_belong_to_many(:organizations)
+  end
+
+  it "has many tickets" do
+    expect(ResourceCategory.new).to have_many(:tickets)
+  end
+
 end
