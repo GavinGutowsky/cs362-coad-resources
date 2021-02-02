846fcdb71a16b210fa3095339358a87097acb2f8
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-01 22:19:37 -0800

    Creation of User validator presence tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,27 @@ RSpec.describe User, type: :model do
     expect(User.new).to belong_to(:organization).optional
   end
 
+  describe "validators" do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_length_of(:email)
+          .is_at_least(1) 
+          .is_at_most(255)
+          .on(:create) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it "has a valid email format" do
+      VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      user = User.new
+      user.email = "User@gmail.com"
+      expect(user.email).to match(VALID_EMAIL_REGEX)
+      user.email = "us..er#gmail.com"
+      expect(user.email.match(VALID_EMAIL_REGEX)).to be_falsy
+    end
+
+    it { is_expected.to validate_presence_of(:password).on(:create) }
+    it { is_expected.to validate_length_of(:password) 
+          .is_at_least(7) 
+          .is_at_most(255)
+          .on(:create) }
+  end
+
 end
