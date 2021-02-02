7764d977dc8d1476f0d8735854807092c7e3d11e
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-01 20:51:36 -0800

    Fix error: is_expect -> is_expected

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe ResourceCategory, type: :model do
 
   describe "attributes" do
-    it { is_expect.to respond_to(:name) }
-    it { is_expect.to respond_to(:active) }
+    it { is_expected.to respond_to(:name) }
+    it { is_expected.to respond_to(:active) }
   end
 
   describe "associations" do
-    it { is_expect.to have_and_belong_to_many(:organizations) }
-    it { is_expect.to have_many(:tickets) }
+    it { is_expected.to have_and_belong_to_many(:organizations) }
+    it { is_expected.to have_many(:tickets) }
   end
 
 end
