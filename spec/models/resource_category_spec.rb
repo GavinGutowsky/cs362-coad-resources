9c47b79fe4f7f7a646f3b83522f76068514baa44
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-02 14:58:43 -0800

    Creation of ResourceCategory method tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -21,4 +21,33 @@ RSpec.describe ResourceCategory, type: :model do
     it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   end
 
+  describe "methods" do
+    it "finds or creates an object with name unspecified" do
+      expect(ResourceCategory.unspecified.name).to eq('Unspecified')
+    end
+
+    it "can be activated" do
+      rc = ResourceCategory.new(active: false)
+      rc.activate
+      expect(rc.active).to be_truthy
+    end
+
+    it "can be deactivated" do
+      rc = ResourceCategory.new(active: true)
+      rc.deactivate
+      expect(rc.active).to be_falsy
+    end
+
+    it "is inactive when its active attribute is set to false" do
+      rc = ResourceCategory.new(active: false)
+      expect(rc.inactive?).to be_truthy
+      rc.active = true
+      expect(rc.inactive?).to be_falsy
+    end
+
+    it "has a string representation that is its name" do
+      expect(ResourceCategory.new(name: 'Fake').to_s).to eq('Fake')
+    end
+  end
+
 end
