6fe959aa7bbfa2730332d1ce6845d416ec912745
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-01 20:55:11 -0800

    Creation of ResourceCategory model validator presence tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe ResourceCategory, type: :model do
     it { is_expected.to have_many(:tickets) }
   end
 
+  describe "validators" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name)
+          .is_at_least(1) 
+          .is_at_most(255)
+          .on(:create) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  end
+
 end
