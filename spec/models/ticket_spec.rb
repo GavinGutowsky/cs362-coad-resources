946df2291b92c3652a43ac1bed3f7ba29d0714d3
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-01 20:29:13 -0800

    Refactor Ticket model tests to DRY object instantiation

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,36 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-  it "has a name" do
-    expect(Ticket.new).to respond_to(:name)
+  describe "attributes" do
+    it { is_expected.to respond_to(:name) }
+    it { is_expected.to respond_to(:description) }
+    it { is_expected.to respond_to(:phone) }
+    it { is_expected.to respond_to(:closed)}
+    it { is_expected.to respond_to(:closed_at) }
   end
 
-  it "has a description" do
-    expect(Ticket.new).to respond_to(:description)
-  end
-
-  it "has a phone" do
-    expect(Ticket.new).to respond_to(:phone)
-  end
-
-  it "has a closed attribute" do
-    expect(Ticket.new).to respond_to(:closed)
-  end
-
-  it "has a closed_at attribute" do
-    expect(Ticket.new).to respond_to(:closed_at)
-  end
-
-  it "belongs to a region" do
-    expect(Ticket.new).to belong_to(:region)
-  end
-
-  it "belongs to a resource_category" do
-    expect(Ticket.new).to belong_to(:resource_category)
-  end
-
-  it "belongs to a organization" do
-    expect(Ticket.new).to belong_to(:organization).optional
+  describe "associations" do
+    it { is_expected.to belong_to(:region) }
+    it { is_expected.to belong_to(:resource_category) }
+    it { is_expected.to belong_to(:organization).optional }
   end
 
 end
