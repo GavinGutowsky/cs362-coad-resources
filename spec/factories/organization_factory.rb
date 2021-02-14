f338de48e8a7f1170d439075f63ab603fee3dffe
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-14 15:05:53 -0800

    Organization factory body

--- spec/factories/organization_factory.rb
+++ spec/factories/organization_factory.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :organization do
-  
+  name { 'Fake' }
+  status { 'approved' }
+  phone { '541-330-8612' }
+  email { 'fake@email.com' }
+  description { 'Fake description' }
+  rejection_reason { 'Fake reason' }
+  liability_insurance { true }
+  primary_name { 'Fake primary name' }
+  secondary_name { 'Fake secondary name' }
+  title { 'Fake title' }
+  transportation { :yes }
   end
 end
