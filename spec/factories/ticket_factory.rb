f8f333496ca460aa99a455b6c9f5f95ce1756264
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-14 16:05:46 -0800

    Refactor ticket factory, add relations

--- spec/factories/ticket_factory.rb
+++ spec/factories/ticket_factory.rb
@@ -2,15 +2,17 @@ FactoryBot.define do
   factory :ticket do
     name { 'Fake' }
     description { 'Fake description' }
-    phone { '541-330-8612' }
+    phone { '+1 541-330-8612' }
     closed { true }
+    region 
+    resource_category 
   end
 
   trait :closed do
-    closed: true
+    closed { true }
   end
 
   trait :open do
-    closed: false
+    closed { false }
   end
 end
