863e6543d361f4434e2d58473fe4f778782f34e6
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-14 16:46:24 -0800

    Update resource category factory with relations

--- spec/factories/resource_category_factory.rb
+++ spec/factories/resource_category_factory.rb
@@ -1,6 +1,15 @@
 FactoryBot.define do
   factory :resource_category do
-    name { 'Fake' }
+    name { 'Fake Resource Category' }
     active { false }
+
+    trait :active do
+      active { true }
+    end
+
+    trait :deactive do
+      active { false }
+    end
+  
   end
 end
