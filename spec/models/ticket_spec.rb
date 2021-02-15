9b7fe1affa555b5aebb65fc218bd10d0a811daba
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-14 19:51:12 -0800

    Updated wording of ticket scope test descriptions

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -41,11 +41,11 @@ RSpec.describe Ticket, type: :model do
     let(:open_organization_ticket) { create(:ticket, :open, :organization) }
 
     describe 'open' do
-      it 'contains open tickets not belonging to an organization' do
+      it 'contains open tickets that do not belong to an organization' do
         expect(Ticket.open).to include(open_nil_ticket)
       end
 
-      it 'does not contain open tickets belonging to an organization' do
+      it 'does not contain open tickets that belong to an organization' do
         expect(Ticket.open).to_not include(open_organization_ticket)
       end
       
@@ -65,7 +65,7 @@ RSpec.describe Ticket, type: :model do
     end
 
     describe 'all_organization' do
-      it 'contains all open tickets belonging to an organization' do
+      it 'contains open tickets that belong to an organization' do
         expect(Ticket.all_organization).to include(open_organization_ticket)
       end
 
@@ -73,64 +73,64 @@ RSpec.describe Ticket, type: :model do
         expect(Ticket.all_organization).to_not include(closed_ticket)
       end
 
-      it 'does not contain open tickets not belonging to an organization' do
+      it 'does not contain open tickets that do not belong to an organization' do
         expect(Ticket.all_organization).to_not include(open_nil_ticket)
       end
     end
 
     describe 'organization' do
-      it 'contains open tickets belonging to a given organization' do
+      it 'contains open tickets that belong to a given organization' do
         organization = open_organization_ticket.organization
         expect(Ticket.organization(organization.id)).to include(open_organization_ticket)
       end
 
-      it 'does not contain closed tickets belonging to a given organization' do
+      it 'does not contain closed tickets that belong to a given organization' do
         organization = closed_organization_ticket.organization
         expect(Ticket.organization(organization.id)).to_not include(closed_organization_ticket)
       end
       
-      it 'does not contain tickets not belonging to a given organization' do
+      it 'does not contain tickets that do not belong to a given organization' do
         organization = open_organization_ticket.organization
         expect(Ticket.organization(organization.id)).to_not include(open_ticket)
       end
     end
 
     describe 'closed_organization' do
-      it 'contains closed tickets belonging to a given organization' do
+      it 'contains closed tickets that belong to a given organization' do
         organization = closed_organization_ticket.organization
         expect(Ticket.closed_organization(organization.id)).to include(closed_organization_ticket)
       end
 
-      it 'does not contain open tickets belonging to a given organization' do
+      it 'does not contain open tickets that belong to a given organization' do
         organization = open_organization_ticket.organization
         expect(Ticket.closed_organization(organization.id)).to_not include(open_organization_ticket)
       end
       
-      it 'does not contain tickets not belonging to a given organization' do
+      it 'does not contain tickets that do not belong to a given organization' do
         organization = closed_organization_ticket.organization
         expect(Ticket.closed_organization(organization.id)).to_not include(closed_ticket)
       end
     end
 
     describe 'region' do
-      it 'contains tickets belonging to a given region' do
+      it 'contains tickets that belong to a given region' do
         region = open_ticket.region
         expect(Ticket.region(region.id)).to include(open_ticket)
       end
 
-      it 'does not contain tickets not belonging to a given region' do
+      it 'does not contain tickets that do not belong to a given region' do
         region = open_ticket.region
         expect(Ticket.region(region.id)).to_not include(closed_ticket)
       end
     end
 
     describe 'resource_category' do
-      it 'contains tickets belonging to a given resource_category' do
+      it 'contains tickets that belong to a given resource_category' do
         resource_category = open_ticket.resource_category
         expect(Ticket.resource_category(resource_category.id)).to include(open_ticket)
       end
 
-      it 'does not contain tickets not belonging to a given resource_category' do
+      it 'does not contain tickets that do not belong to a given resource_category' do
         resource_category = open_ticket.resource_category
         expect(Ticket.resource_category(resource_category.id)).to_not include(closed_ticket)
       end
