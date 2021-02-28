86960124d491b1bfa365454f5ae4e4a0a7662051
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-28 13:21:47 -0800

    Creation of regions controller tests

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
+  context 'unauthenticated users' do
+    it 'redirects to sign in screen' do
+      get :index
+      expect(response).to redirect_to(new_user_session_url)
+      get :show, params: { id: 'Fake' }
+      expect(response).to redirect_to(new_user_session_url)
+      get :new
+      expect(response).to redirect_to(new_user_session_url)
+      post :create
+      expect(response).to redirect_to(new_user_session_url)
+      get :edit, params: { id: 'Fake' }
+      expect(response).to redirect_to(new_user_session_url)
+      put :update, params: { id: 'Fake' }
+      expect(response).to redirect_to(new_user_session_url)
+      delete :destroy, params: { id: 'Fake' }
+      expect(response).to redirect_to(new_user_session_url)
+    end
+  end
 
+  context 'organization users' do
+    before do
+      organization_user = build(:user, :organization)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(organization_user)
+      allow(controller).to receive(:current_user).and_return(organization_user)
+    end
+
+    it 'redirects to dashboard' do
+      get :index
+      expect(response).to redirect_to(dashboard_url)
+      get :show, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+      get :new
+      expect(response).to redirect_to(dashboard_url)
+      post :create
+      expect(response).to redirect_to(dashboard_url)
+      get :edit, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+      put :update, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+      delete :destroy, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+  end
 end
