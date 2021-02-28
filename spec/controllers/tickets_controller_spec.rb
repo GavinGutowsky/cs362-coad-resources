d5a3cc6106524787812ffcbe13ffa3854a99925a
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-02-28 14:15:16 -0800

    Creation of ticket controller tests (unauthenticated, approved organization, and admin user redirects)

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
+  context 'unauthenticated users' do
+    before do
+      allow(controller).to receive(:current_user).and_return(build(:user))
+    end
 
+    it 'redirects to dashboard' do
+      get :show, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+      post :capture, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+      post :release, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+      patch :close, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+      delete :destroy, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+  end
+
+  context 'approved organization users' do
+    before do
+      organization_user = build(:user, :approved_organization)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(organization_user)
+      allow(controller).to receive(:current_user).and_return(organization_user)
+    end
+
+    it 'redirects to dashboard' do
+      delete :destroy, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+  end
+
+  context 'admin users' do
+    before do
+      admin_user = build(:user, :admin)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(admin_user)
+      allow(controller).to receive(:current_user).and_return(admin_user)
+    end
+
+    it 'redirects to dashboard' do
+      post :capture, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+      post :release, params: { id: 'Fake' }
+      expect(response).to redirect_to(dashboard_url)
+    end
+  end
 end
