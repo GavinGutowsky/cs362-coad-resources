bf659b1a582126049324c0ed03d295b24be6a4cc
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-03-02 11:43:38 -0800

    Creation of dashboard controller index test (unauthenticated user)

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-
+  context 'unauthenticated users' do
+    it 'redirects to dashboard' do
+      get :index
+      expect(response).to redirect_to(new_user_session_url)
+    end
+  end
 end
