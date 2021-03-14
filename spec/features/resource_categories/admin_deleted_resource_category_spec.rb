1fc572bda9610f7ff50fc2f9d0d465a0fd2c6899
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-03-13 19:16:18 -0800

    Deleting a resource category feature success test

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
+  
+  let(:admin) do
+    admin = create(:user, :admin)
+    admin.confirm
+    return admin
+  end
+
+  specify 'succeeds' do
+    ResourceCategory.create(name: 'Fake Category')
+    log_in_as(admin)
+    click_on 'Categories'
+    click_on 'Fake Category'
+    click_on 'Delete'
+    expect(page.body).not_to have_selector(:link_or_button, 'Fake Category')
+  end
 
 end
