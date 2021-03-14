cf1262ebf407ef8518203bc925334478b68b4628
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-03-13 16:29:08 -0800

    Dashboard helper: admin user test

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe DashboardHelper, type: :helper do
   describe 'dashboard for' do
      it 'returns dashboard for admin user' do
-       
+       admin_user = double
+       allow(admin_user).to receive(:admin?).and_return(true)
+       expect(dashboard_for(admin_user)).to eq('admin_dashboard')
      end
 
      it 'returns dashboard for submitted organization user' do
@@ -16,6 +18,6 @@ RSpec.describe DashboardHelper, type: :helper do
 
      it 'returns dashboard for other users' do
        
-     ends
+     end
    end
 end
