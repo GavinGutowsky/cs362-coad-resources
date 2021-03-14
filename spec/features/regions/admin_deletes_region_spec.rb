196fd72b71b5373b70385fe53ac6e08b3baed51f
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-03-13 19:23:24 -0800

    Deleting a region feature success test

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
 
+  let(:admin) do
+    admin = create(:user, :admin)
+    admin.confirm
+    return admin
+  end
+
+  specify 'succeeds' do
+    Region.create(name: 'Fake Region')
+    log_in_as(admin)
+    click_on 'Regions'
+    click_on 'Fake Region'
+    click_on 'Delete'
+    expect(page.body).not_to have_selector(:link_or_button, 'Fake Region')
+  end
+
 end
