5e66e95a70c230528de39f16bc1421b47c45263c
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-03-13 16:57:39 -0800

    Dashboard helper: Change spacing for test readability

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe DashboardHelper, type: :helper do
      it 'returns dashboard for admin user' do
        admin_user = double
        allow(admin_user).to receive(:admin?).and_return(true)
+       
        expect(dashboard_for(admin_user)).to eq('admin_dashboard')
      end
 
@@ -12,6 +13,7 @@ RSpec.describe DashboardHelper, type: :helper do
        submitted_organization_user = double
        allow(submitted_organization_user).to receive(:admin?).and_return(false)
        allow(submitted_organization_user).to receive_message_chain('organization.submitted?').and_return(true)
+      
        expect(dashboard_for(submitted_organization_user)).to eq('organization_submitted_dashboard')
      end
 
@@ -20,6 +22,7 @@ RSpec.describe DashboardHelper, type: :helper do
        allow(approved_organization_user).to receive(:admin?).and_return(false)
        allow(approved_organization_user).to receive_message_chain('organization.submitted?').and_return(false)
        allow(approved_organization_user).to receive_message_chain('organization.approved?').and_return(true)
+       
        expect(dashboard_for(approved_organization_user)).to eq('organization_approved_dashboard')
      end
 
@@ -28,6 +31,7 @@ RSpec.describe DashboardHelper, type: :helper do
        allow(other_user).to receive(:admin?).and_return(false)
        allow(other_user).to receive_message_chain('organization.submitted?').and_return(false)
        allow(other_user).to receive_message_chain('organization.approved?').and_return(false)
+       
        expect(dashboard_for(other_user)).to eq('create_application_dashboard')
      end
    end
