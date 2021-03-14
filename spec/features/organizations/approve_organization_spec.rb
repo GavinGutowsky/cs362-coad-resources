3b9e70ab79b0705d16740d966c951f84cdbedec5
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-03-14 12:03:54 -0700

    Rejecting an organization success test

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
 
+  let(:admin) do
+    admin = create(:user, :admin)
+    admin.confirm
+    return admin
+  end
+
+  specify 'succeeds' do
+    organization = create(:organization)
+    log_in_as(admin)
+    click_on 'Organizations'
+    click_on 'Pending'
+    click_on organization.name
+    click_on 'Approve'
+    expect(page.body).to have_text('approved')
+  end
+
 end
