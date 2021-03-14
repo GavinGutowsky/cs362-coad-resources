f7ac0386281a052b55644c1ff8105757975f35a1
Gavin Gutowsky <46430132+GavinGutowsky@users.noreply.github.com>
2021-03-13 20:23:00 -0800

    Deleting a ticket success test

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
 
+  let(:admin) do
+    admin = create(:user, :admin)
+    admin.confirm
+    return admin
+  end
+
+  specify 'succeeds' do
+    ticket = create(:ticket)
+    log_in_as(admin)
+    click_on ticket.name
+    click_on 'Delete'
+    expect(page.body).not_to have_selector(:link_or_button, ticket.name)
+  end
+
 end
